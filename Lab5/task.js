c5dc87ef5610a19e231f61cdaabbec719c637200
5ToYou <sobkos934@gmail.com>
2025-11-02 21:30:29 +0200

    the last task is 6

--- Lab5/task.js
+++ Lab5/task.js
@@ -42,7 +42,13 @@ console.log(isAnagram("listen", "silent"));
 
 //4.
 function twoSum(arr, target){
-
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return [i, j];
+            }
+        }
+    }
 }
 console.log(twoSum([2, 7, 11, 15], 9));
 
