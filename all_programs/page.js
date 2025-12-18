78c3ddf9beb14c4380292d287176d4e527d0efa3
5ToYou <sobkos934@gmail.com>
2025-12-18 09:40:07 +0200

    anim

--- all_programs/page.js
+++ all_programs/page.js
@@ -9,16 +9,18 @@ document.getElementById("goto_index").onclick = () => {change_HTML("index.html")
 const colorful_case = document.querySelector(".colorful_case")
 
 const min_number = 0;
-const max_number = 255;
+const max_number = 360;
 
 
 function my_func(element){
-    r1 = Math.floor(Math.random() * (max_number - min_number + 1));
-    g2 = Math.floor(Math.random() * (max_number - min_number + 1));
-    b3 = Math.floor(Math.random() * (max_number - min_number + 1));
-    const rgb_color = `rgb(${r1},${g2},${b3})`;
+    random_color = Math.floor(Math.random() * (max_number - min_number + 1));
+    const rgb_color = `hsl(${random_color},100%,50%)`;
 
-    element.style.color = rgb_color;
+    element.addEventListener("animationend",() => {
+        element.classList.remove("anim")
+    }) 
+    element.style = `--dynamic-1comma:${rgb_color}`;
+    element.classList.add("anim")
 }
 
 function blacker(element){
